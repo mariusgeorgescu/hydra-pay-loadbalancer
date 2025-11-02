7ab59bfa601bbab85c2d4d392f256da4331f8f57
Marius Georgescu <georgescumarius@live.com>
2025-11-02 13:03:02 +0200

    2 nov

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,7 @@ import MyLib
 main :: IO ()
 main = do
   result <- runHydraClient "http://127.0.0.1:3001" $ 
-    queryFunds "addr_test1qzl4ugvgu920q4lr86lydvfqstm42arezeyjm0wy8mujvm93dzr5pcgc37ym773c8w6q5uem08ts6qskrvc6mw6nvf4s4tu94z"
+    queryFunds "addr_test1qprg7rzdsm2ml82499utezhsl43sz8nr39sd2t8djefhd7xjgx8m3d6keddfp04fr59vw2k0curt6jtss6evepvj5yjsa8wwlv"
   case result of
     Left err -> print err
     Right funds -> print funds
--- src/HydraPay/API/Types.hs
+++ src/HydraPay/API/Types.hs
@@ -5,231 +5,298 @@
 
 module HydraPay.API.Types
   ( -- * Core Types
-    TxOutRef(..)
-  , DepositSchema(..)
-  , WithdrawSchema(..)
-  , PayMerchantSchema(..)
-  , ManageHeadSchema(..)
-  , QueryFundsResponse(..)
-  , TxBuiltResponse(..)
-  , FundsUtxo(..)
-  
-  -- * Error Types
-  , HydraApiError(..)
-  , AddressNotFoundError(..)
-  , FundsUTxONotFoundError(..)
-  , BadRequest(..)
-  , InternalServerError(..)
-  ) where
+    TxOutRef (..),
+    DepositSchema (..),
+    WithdrawSchema (..),
+    PayMerchantSchema (..),
+    ManageHeadSchema (..),
+    QueryFundsResponse (..),
+    TxBuiltResponse (..),
+    FundsUtxo (..),
 
+    -- * Error Types
+    HydraApiError (..),
+    AddressNotFoundError (..),
+    FundsUTxONotFoundError (..),
+    BadRequest (..),
+    InternalServerError (..),
+  )
+where
+
+import Control.Applicative ((<|>))
 import Data.Aeson
 import Data.Aeson.Types ()
 import Data.Text (Text)
 import Data.Vector (Vector)
-import GHC.Generics
 import Deriving.Aeson ()
-import Control.Applicative ((<|>))
+import GHC.Generics
 
 -- | Transaction output reference
 data TxOutRef = TxOutRef
-  { txOutRefHash :: Text
-  , txOutRefIndex :: Int
-  } deriving (Eq, Show, Generic)
+  { txOutRefHash :: Text,
+    txOutRefIndex :: Int
+  }
+  deriving (Eq, Show, Generic)
 
 instance ToJSON TxOutRef where
-  toJSON = genericToJSON defaultOptions
-    { fieldLabelModifier = camelTo2 '_' . drop 8  -- drop "txOutRef"
-    }
+  toJSON =
+    genericToJSON
+      defaultOptions
+        { fieldLabelModifier = \s -> case s of
+            "txOutRefHash" -> "txHash"
+            "txOutRefIndex" -> "outputIndex"
+            _ -> camelTo2 '_' $ drop 8 s -- drop "txOutRef"
+        }
 
 instance FromJSON TxOutRef where
-  parseJSON = genericParseJSON defaultOptions
-    { fieldLabelModifier = camelTo2 '_' . drop 8
-    }
+  parseJSON =
+    genericParseJSON
+      defaultOptions
+        { fieldLabelModifier = \s -> case s of
+            "txOutRefHash" -> "txHash"
+            "txOutRefIndex" -> "outputIndex"
+            _ -> camelTo2 '_' $ drop 8 s -- drop "txOutRef"
+        }
 
 -- | Funds UTxO with signature
 data FundsUtxo = FundsUtxo
-  { fundsUtxoSignature :: Maybe Text
-  , fundsUtxoRef :: TxOutRef
-  } deriving (Eq, Show, Generic)
+  { fundsUtxoSignature :: Maybe Text,
+    fundsUtxoRef :: TxOutRef
+  }
+  deriving (Eq, Show, Generic)
 
 instance ToJSON FundsUtxo where
-  toJSON = genericToJSON defaultOptions
-    { fieldLabelModifier = camelTo2 '_' . drop 9  -- drop "fundsUtxo"
-    }
+  toJSON =
+    genericToJSON
+      defaultOptions
+        { fieldLabelModifier = camelTo2 '_' . drop 9 -- drop "fundsUtxo"
+        }
 
 instance FromJSON FundsUtxo where
-  parseJSON = genericParseJSON defaultOptions
-    { fieldLabelModifier = camelTo2 '_' . drop 9
-    }
+  parseJSON =
+    genericParseJSON
+      defaultOptions
+        { fieldLabelModifier = camelTo2 '_' . drop 9
+        }
 
 -- | Deposit request schema
 data DepositSchema = DepositSchema
-  { depositUserAddress :: Text
-  , depositPublicKey :: Text
-  , depositAmount :: Vector (Vector Text)
-  , depositFundsUtxoRef :: Maybe TxOutRef
-  } deriving (Eq, Show, Generic)
+  { depositUserAddress :: Text,
+    depositPublicKey :: Text,
+    depositAmount :: Vector (Vector Text),
+    depositFundsUtxoRef :: Maybe TxOutRef
+  }
+  deriving (Eq, Show, Generic)
 
 instance ToJSON DepositSchema where
-  toJSON = genericToJSON defaultOptions
-    { fieldLabelModifier = camelTo2 '_' . drop 7  -- drop "deposit"
-    }
+  toJSON =
+    genericToJSON
+      defaultOptions
+        { fieldLabelModifier = camelTo2 '_' . drop 7 -- drop "deposit"
+        }
 
 instance FromJSON DepositSchema where
-  parseJSON = genericParseJSON defaultOptions
-    { fieldLabelModifier = camelTo2 '_' . drop 7
-    }
+  parseJSON =
+    genericParseJSON
+      defaultOptions
+        { fieldLabelModifier = camelTo2 '_' . drop 7
+        }
 
 -- | Withdraw request schema
 data WithdrawSchema = WithdrawSchema
-  { withdrawAddress :: Text
-  , withdrawOwner :: Text  -- "user" or "merchant"
-  , withdrawFundsUtxos :: Vector FundsUtxo
-  , withdrawNetworkLayer :: Text  -- "L1" or "L2"
-  } deriving (Eq, Show, Generic)
+  { withdrawAddress :: Text,
+    withdrawOwner :: Text, -- "user" or "merchant"
+    withdrawFundsUtxos :: Vector FundsUtxo,
+    withdrawNetworkLayer :: Text -- "L1" or "L2"
+  }
+  deriving (Eq, Show, Generic)
 
 instance ToJSON WithdrawSchema where
-  toJSON = genericToJSON defaultOptions
-    { fieldLabelModifier = camelTo2 '_' . drop 8  -- drop "withdraw"
-    }
+  toJSON =
+    genericToJSON
+      defaultOptions
+        { fieldLabelModifier = camelTo2 '_' . drop 8 -- drop "withdraw"
+        }
 
 instance FromJSON WithdrawSchema where
-  parseJSON = genericParseJSON defaultOptions
-    { fieldLabelModifier = camelTo2 '_' . drop 8
-    }
+  parseJSON =
+    genericParseJSON
+      defaultOptions
+        { fieldLabelModifier = camelTo2 '_' . drop 8
+        }
 
 -- | Pay merchant request schema
 data PayMerchantSchema = PayMerchantSchema
-  { payMerchantMerchantAddress :: Text
-  , payMerchantFundsUtxoRef :: TxOutRef
-  , payMerchantAmount :: Vector (Vector Text)
-  , payMerchantSignature :: Text
-  , payMerchantMerchantFundsUtxo :: Maybe TxOutRef
-  } deriving (Eq, Show, Generic)
+  { payMerchantMerchantAddress :: Text,
+    payMerchantFundsUtxoRef :: TxOutRef,
+    payMerchantAmount :: Vector (Vector Text),
+    payMerchantSignature :: Text,
+    payMerchantMerchantFundsUtxo :: Maybe TxOutRef
+  }
+  deriving (Eq, Show, Generic)
 
 instance ToJSON PayMerchantSchema where
-  toJSON = genericToJSON defaultOptions
-    { fieldLabelModifier = camelTo2 '_' . drop 12  -- drop "payMerchant"
-    }
+  toJSON =
+    genericToJSON
+      defaultOptions
+        { fieldLabelModifier = camelTo2 '_' . drop 12 -- drop "payMerchant"
+        }
 
 instance FromJSON PayMerchantSchema where
-  parseJSON = genericParseJSON defaultOptions
-    { fieldLabelModifier = camelTo2 '_' . drop 12
-    }
+  parseJSON =
+    genericParseJSON
+      defaultOptions
+        { fieldLabelModifier = camelTo2 '_' . drop 12
+        }
 
 -- | Manage head request schema
 newtype ManageHeadSchema = ManageHeadSchema
   { manageHeadPeerApiUrls :: Vector Text
-  } deriving (Eq, Show, Generic)
+  }
+  deriving (Eq, Show, Generic)
 
 instance ToJSON ManageHeadSchema where
-  toJSON = genericToJSON defaultOptions
-    { fieldLabelModifier = camelTo2 '_' . drop 11  -- drop "manageHead"
-    }
+  toJSON =
+    genericToJSON
+      defaultOptions
+        { fieldLabelModifier = camelTo2 '_' . drop 11 -- drop "manageHead"
+        }
 
 instance FromJSON ManageHeadSchema where
-  parseJSON = genericParseJSON defaultOptions
-    { fieldLabelModifier = camelTo2 '_' . drop 11
-    }
+  parseJSON =
+    genericParseJSON
+      defaultOptions
+        { fieldLabelModifier = camelTo2 '_' . drop 11
+        }
 
 -- | Query funds response
 data QueryFundsResponse = QueryFundsResponse
-  { queryFundsFundsInL1 :: Vector TxOutRef
-  , queryFundsFundsInL2 :: Vector TxOutRef
-  , queryFundsTotalInL1 :: Value  -- Generic JSON object
-  , queryFundsTotalInL2 :: Value  -- Generic JSON object
-  } deriving (Eq, Show, Generic)
+  { queryFundsFundsInL1 :: Vector TxOutRef,
+    queryFundsFundsInL2 :: Vector TxOutRef,
+    queryFundsTotalInL1 :: Value, -- Generic JSON object
+    queryFundsTotalInL2 :: Value -- Generic JSON object
+  }
+  deriving (Eq, Show, Generic)
 
 instance ToJSON QueryFundsResponse where
-  toJSON = genericToJSON defaultOptions
-    { fieldLabelModifier = camelTo2 '_' . drop 10  -- drop "queryFunds"
-    }
+  toJSON =
+    genericToJSON
+      defaultOptions
+        { fieldLabelModifier = camelTo2 '_' . drop 10 -- drop "queryFunds"
+        }
 
 instance FromJSON QueryFundsResponse where
-  parseJSON = genericParseJSON defaultOptions
-    { fieldLabelModifier = \s -> case s of
-        "queryFundsFundsInL1" -> "fundsInL1"
-        "queryFundsFundsInL2" -> "fundsInL2" 
-        "queryFundsTotalInL1" -> "totalInL1"
-        "queryFundsTotalInL2" -> "totalInL2"
-        _ -> camelTo2 '_' $ drop 10 s  -- drop "queryFunds"
-    }
+  parseJSON =
+    genericParseJSON
+      defaultOptions
+        { fieldLabelModifier = \s -> case s of
+            "queryFundsFundsInL1" -> "fundsInL1"
+            "queryFundsFundsInL2" -> "fundsInL2"
+            "queryFundsTotalInL1" -> "totalInL1"
+            "queryFundsTotalInL2" -> "totalInL2"
+            _ -> camelTo2 '_' $ drop 10 s -- drop "queryFunds"
+        }
 
 -- | Transaction built response
 data TxBuiltResponse = TxBuiltResponse
-  { txBuiltCborHex :: Text
-  , txBuiltFundsUtxoRef :: TxOutRef
-  } deriving (Eq, Show, Generic)
+  { txBuiltCborHex :: Text,
+    txBuiltFundsUtxoRef :: TxOutRef
+  }
+  deriving (Eq, Show, Generic)
 
 instance ToJSON TxBuiltResponse where
-  toJSON = genericToJSON defaultOptions
-    { fieldLabelModifier = camelTo2 '_' . drop 7  -- drop "txBuilt"
-    }
+  toJSON =
+    genericToJSON
+      defaultOptions
+        { fieldLabelModifier = camelTo2 '_' . drop 7 -- drop "txBuilt"
+        }
 
 instance FromJSON TxBuiltResponse where
-  parseJSON = genericParseJSON defaultOptions
-    { fieldLabelModifier = camelTo2 '_' . drop 7
-    }
+  parseJSON =
+    genericParseJSON
+      defaultOptions
+        { fieldLabelModifier = camelTo2 '_' . drop 7
+        }
 
 -- | Address not found error
 newtype AddressNotFoundError = AddressNotFoundError
   { addressNotFoundMessage :: Text
-  } deriving (Eq, Show, Generic)
+  }
+  deriving (Eq, Show, Generic)
 
 instance ToJSON AddressNotFoundError where
-  toJSON = genericToJSON defaultOptions
-    { fieldLabelModifier = camelTo2 '_' . drop 15  -- drop "addressNotFound"
-    }
+  toJSON =
+    genericToJSON
+      defaultOptions
+        { fieldLabelModifier = camelTo2 '_' . drop 15 -- drop "addressNotFound"
+        }
 
 instance FromJSON AddressNotFoundError where
-  parseJSON = genericParseJSON defaultOptions
-    { fieldLabelModifier = camelTo2 '_' . drop 15
-    }
+  parseJSON =
+    genericParseJSON
+      defaultOptions
+        { fieldLabelModifier = camelTo2 '_' . drop 15
+        }
 
 -- | Funds UTxO not found error
 newtype FundsUTxONotFoundError = FundsUTxONotFoundError
   { fundsUTxONotFoundMessage :: Text
-  } deriving (Eq, Show, Generic)
+  }
+  deriving (Eq, Show, Generic)
 
 instance ToJSON FundsUTxONotFoundError where
-  toJSON = genericToJSON defaultOptions
-    { fieldLabelModifier = camelTo2 '_' . drop 19  -- drop "fundsUTxONotFound"
-    }
+  toJSON =
+    genericToJSON
+      defaultOptions
+        { fieldLabelModifier = camelTo2 '_' . drop 19 -- drop "fundsUTxONotFound"
+        }
 
 instance FromJSON FundsUTxONotFoundError where
-  parseJSON = genericParseJSON defaultOptions
-    { fieldLabelModifier = camelTo2 '_' . drop 19
-    }
+  parseJSON =
+    genericParseJSON
+      defaultOptions
+        { fieldLabelModifier = camelTo2 '_' . drop 19
+        }
 
 -- | Bad request error
 newtype BadRequest = BadRequest
   { badRequestMessage :: Text
-  } deriving (Eq, Show, Generic)
+  }
+  deriving (Eq, Show, Generic)
 
 instance ToJSON BadRequest where
-  toJSON = genericToJSON defaultOptions
-    { fieldLabelModifier = camelTo2 '_' . drop 10  -- drop "badRequest"
-    }
+  toJSON =
+    genericToJSON
+      defaultOptions
+        { fieldLabelModifier = camelTo2 '_' . drop 10 -- drop "badRequest"
+        }
 
 instance FromJSON BadRequest where
-  parseJSON = genericParseJSON defaultOptions
-    { fieldLabelModifier = camelTo2 '_' . drop 10
-    }
+  parseJSON =
+    genericParseJSON
+      defaultOptions
+        { fieldLabelModifier = camelTo2 '_' . drop 10
+        }
 
 -- | Internal server error
 newtype InternalServerError = InternalServerError
   { internalServerErrorMessage :: Text
-  } deriving (Eq, Show, Generic)
+  }
+  deriving (Eq, Show, Generic)
 
 instance ToJSON InternalServerError where
-  toJSON = genericToJSON defaultOptions
-    { fieldLabelModifier = camelTo2 '_' . drop 20  -- drop "internalServerError"
-    }
+  toJSON =
+    genericToJSON
+      defaultOptions
+        { fieldLabelModifier = camelTo2 '_' . drop 20 -- drop "internalServerError"
+        }
 
 instance FromJSON InternalServerError where
-  parseJSON = genericParseJSON defaultOptions
-    { fieldLabelModifier = camelTo2 '_' . drop 20
-    }
+  parseJSON =
+    genericParseJSON
+      defaultOptions
+        { fieldLabelModifier = camelTo2 '_' . drop 20
+        }
 
 -- | Custom error type wrapping all API errors
 data HydraApiError
@@ -237,7 +304,7 @@ data HydraApiError
   | HydraFundsUTxONotFound FundsUTxONotFoundError
   | HydraBadRequest BadRequest
   | HydraInternalServerError InternalServerError
-  | HydraClientError Text  -- For client-side errors
+  | HydraClientError Text -- For client-side errors
   deriving (Eq, Show)
 
 instance ToJSON HydraApiError where
@@ -249,10 +316,10 @@ instance ToJSON HydraApiError where
     HydraClientError msg -> object ["message" .= msg]
 
 instance FromJSON HydraApiError where
-  parseJSON v@(Object obj) = 
-    HydraAddressNotFound <$> parseJSON v <|>
-    HydraFundsUTxONotFound <$> parseJSON v <|>
-    HydraBadRequest <$> parseJSON v <|>
-    HydraInternalServerError <$> parseJSON v <|>
-    HydraClientError <$> (obj .: "message")
+  parseJSON v@(Object obj) =
+    HydraAddressNotFound <$> parseJSON v
+      <|> HydraFundsUTxONotFound <$> parseJSON v
+      <|> HydraBadRequest <$> parseJSON v
+      <|> HydraInternalServerError <$> parseJSON v
+      <|> HydraClientError <$> (obj .: "message")
   parseJSON _ = fail "Expected JSON object"
